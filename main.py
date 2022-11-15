04257ff92cdcdd711323530483cbdcbdce8fc8fa
imulki <imamnurulhukmi@gmail.com>
2022-11-15 09:50:24 +0700

    integrate web with prediction model

--- main.py
+++ main.py
@@ -2,10 +2,16 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
 from flask import Flask, request, redirect, url_for, render_template
+from sklearn.neural_network import MLPClassifier
+import pickle
  
 # Flask constructor takes the name of
 # current module (__name__) as argument.
 app = Flask(__name__)
+
+clf: MLPClassifier = None
+with open('ml.pickle', 'rb') as f:
+  clf = pickle.load(f)
  
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
@@ -24,10 +30,19 @@ def success(result):
 @app.route('/predict', methods=['POST'])
 def login():
   if request.method == 'POST':
-    radius = int(request.form['radius'])
-    prediction = 0
-    if (radius > 2):
-      prediction = 1
+    field_list = ["radius", "texture", "perimeter", "area", "smoothness", "compactness", "symmetry", "fractal_dimension"]
+    data = {}
+    model_input = []
+    for field in field_list:
+      x = float(request.form[field])
+      data[field] = x
+      model_input.append(x)
+
+    # radius = int(request.form['radius'])
+    # prediction = 0
+    # if (radius > 2):
+    #   prediction = 1
+    prediction = clf.predict([model_input])[0]
     return redirect(url_for('success', result=prediction))
     
  
